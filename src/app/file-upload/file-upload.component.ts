95bed32db205affd251db90944f6dda3dbd87fbd
Amr Gaber <amrtgaber@gmail.com>
2017-01-05 17:01:23 -0600

    basic error checking and initial file read

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -6,27 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
-  private _filename: string;
+  private _file: File;
+  private _contents: string = '';
+  private _fileReader: FileReader = new FileReader();
 
-  constructor() {
-    this._filename = '';
+  get file() {
+    return this._file;
   }
 
-  get filename() {
-    return this._filename;
+  set file(file) {
+    this._file = file;
   }
 
-  set filename(newFilename) {
-    this._filename = newFilename;
+  get contents() {
+    return this._contents;
   }
 
-  fileUpload(event) {
-    let files = event.srcElement.files;
-    console.log(files);
-    this.filename = files[0].name;
+  set contents(contents) {
+    this._contents = contents;
+  }
+
+  get fileReader() {
+    return this._fileReader;
+  }
+
+  fileUpload(files) {
+    // if file doesn't exist abort
+    if( !files || !files[0] ) {
+      console.warn('No file uploaded');
+      return;
+    }
+
+    this.file = files[0];
+
+    // very simple check for chummer file type
+    if( !this.file.name.endsWith('.chum5') ) {
+      console.error('Not a chummer file');
+      return;
+    }
+
+    // when reading is complete will fire event listener "loadend"
+    this.fileReader.readAsText(this.file);
   }
 
   ngOnInit() {
+    this.fileReader.addEventListener("loadend", () => {
+      // @TODO: send to xml to json conversion service
+      this.contents = this.fileReader.result;
+    });
   }
-
 }
